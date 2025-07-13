eb084155591dc23c8c06f0d181203a121ca11d9d
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-13 22:02:19 +0600

    feat: initial commit of LivePulse real-time Spring Boot application
    - Создан основной класс приложения LivePulseApplication
    - Настроен Spring Boot для запуска real-time сервиса
    - Подготовлена базовая структура проекта для дальнейшей разработки функционала WebSocket и live-обновлений

---
+++ src/main/java/com/example/livepulse_realtime_app/LivePulseApplication.java
@@ -0,0 +1,13 @@
+package com.example.livepulse_realtime_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivePulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivePulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/livepulse_realtime_app/LivePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livepulse_realtime_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivePulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
