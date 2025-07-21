086ea815142687371438d0d76ac6793f550073b0
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-21 21:44:05 +0600

    feat: добавлена сущность User с полями fullName, userName, password и статусом

---
+++ src/main/java/com/example/livepulse_realtime_app/User/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.livepulse_realtime_app.User.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table (name = "usr")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+    private String userName;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
