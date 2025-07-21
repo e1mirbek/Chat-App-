ce36c258b511af8f40f748b83d50f8174351e0be
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-21 21:43:30 +0600

    feat: добавлен UserRepository с методами поиска по статусу и имени пользователя

---
+++ src/main/java/com/example/livepulse_realtime_app/User/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.livepulse_realtime_app.User.repository;
+
+
+import com.example.livepulse_realtime_app.User.entity.Status;
+import com.example.livepulse_realtime_app.User.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List <User> findAllByStatus (Status status);
+
+    User findAllByUsername (String username);
+
+}
