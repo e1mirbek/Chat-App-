704a6e55a2407c8ba58c54b0eb986d165e1f5625
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-24 21:30:22 +0600

    feat: implement UserService with basic logic for user status and persistence

---
+++ src/main/java/com/example/livepulse_realtime_app/User/service/implementation/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.livepulse_realtime_app.User.service.implementation;
+
+
+import com.example.livepulse_realtime_app.User.entity.Status;
+import com.example.livepulse_realtime_app.User.entity.User;
+import com.example.livepulse_realtime_app.User.repository.UserRepository;
+import com.example.livepulse_realtime_app.User.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void saveUser(User user) {
+
+        user.setStatus(Status.ONLINE);
+        userRepository.save(user);
+
+    }
+
+    @Override
+    public void disconnect(User user) {
+
+        var storedUser = userRepository.findAllByUsername(user.getUserName());
+
+        if (storedUser != null && storedUser.getStatus() != Status.ONLINE) {
+
+            storedUser.setStatus(Status.OFFLINE);
+
+            userRepository.save(storedUser);
+
+        }
+    }
+
+    @Override
+    public List<User> findAllByStatus(Status status) {
+        return userRepository.findAllByStatus(Status.ONLINE);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findAllByUsername(username);
+    }
+}
