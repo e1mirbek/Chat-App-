9cd5abc60b9e0114a977d41f5dc9328361ca3be9
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-07-25 15:54:20 +0600

    feat: add UserController with WebSocket endpoints

---
+++ src/main/java/com/example/livepulse_realtime_app/User/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.livepulse_realtime_app.User.controller;
+
+
+import com.example.livepulse_realtime_app.User.entity.User;
+import com.example.livepulse_realtime_app.User.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @MessageMapping ("/user.addUser")
+    @SendTo("/user/public")
+    public User addUser (@Payload User user) {
+        User existingUser = userService.findByUsername(user.getUsername());
+
+        if (existingUser == null) {
+            userService.saveUser(user);
+        }
+
+        return user;
+
+    }
+
+    @MessageMapping ("/user.disconnectUser")
+    @SendTo("/user/public")
+    public User disconnectUser (@Payload User user) {
+        userService.disconnect(user);
+        return user;
+    }
+
+    @GetMapping ("/users")
+    public ResponseEntity<List<User>> findConnectedUsers () {
+        return ResponseEntity.ok(userService.findAllByStatus());
+    }
+
+
+
+}
