7728fb1c34016cd4bc870287d2a851826eef64ad
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-08-03 21:07:09 +0600

    feat: add UserRepository interface with custom query methods

--- src/main/java/com/example/livepulse_realtime_app/User/repository/UserRepository.java
+++ src/main/java/com/example/livepulse_realtime_app/User/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     List <User> findAllByStatus (Status status);
 
