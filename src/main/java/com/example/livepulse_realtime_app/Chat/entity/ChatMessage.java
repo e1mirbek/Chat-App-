60d65bc0dabfceb2e25697b11ac4a99f29a1cfca
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-08-03 21:05:25 +0600

    feat: add ChatMessage entity with JPA annotations

---
+++ src/main/java/com/example/livepulse_realtime_app/Chat/entity/ChatMessage.java
@@ -0,0 +1,30 @@
+package com.example.livepulse_realtime_app.Chat.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table (name = "chat_message")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private  String chatId;
+    private String senderId;
+    private String recipientId;
+    private String content;
+    private Date timestamp;
+
+}
